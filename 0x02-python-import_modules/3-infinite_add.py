22caeab06e1123808f135de439b6d66a3713985f
Blackburn0 <hamzatadebayo5@gmail.com>
2022-12-02 05:17:51 +0100

    first commit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
+if __name__ == "__main__":
     from sys import argv
-total = 0
-for i in range(1, len(argv)):
-    total += int(argv[i])
-print("{:d}".format(total))
+    print('{:d}'.format(sum(int(a) for a in argv[1:])))
+#    result = 0
+#    for arg in argv[1:]:
+#        result += int(arg)
+#    print('{:d}'.format(result))
