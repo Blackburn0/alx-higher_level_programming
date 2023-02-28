53eba1f5fe009825c9b0aef17d92f88819ee3653
Blackburn0 <hamzatadebayo5@gmail.com>
2023-02-28 10:37:32 +0100

    first commit

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,35 +1,44 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jul  3 09:29:55 2020
-
-@author: Robinson Montes
-"""
+"""island perimeter"""
 
 
 def island_perimeter(grid):
-    """
-    Calculater the Island's perimeter
+    """returns perimeter"""
+    count = 0
+    index = 0
 
-    Parameter:
-        grid (array): An 0's and 1's array that represents an island (1)
-        sourrounded by water (0)
+    if not isinstance(grid, list):
+        return 0
 
-    Returns:
-        The island's perimeter
-    """
-    rows = len(grid)
-    cols = len(grid[0])
-    perimeter = 0
-    for i in range(1, rows - 1):
-        for j in range(1, cols - 1):
-            if grid[i][j] == 1:
-                if grid[i - 1][j] == 0:
-                    perimeter += 1
-                if grid[i + 1][j] == 0:
-                    perimeter += 1
-                if grid[i][j - 1] == 0:
-                    perimeter += 1
-                if grid[i][j + 1] == 0:
-                    perimeter += 1
-    return perimeter
+    for row in grid:
+        row_len = len(row)
+    row_len += 2
+
+    grid_line = []
+    i = 0
+    while i < row_len:
+        grid_line.append(0)
+        i += 1
+    for row in grid:
+        grid_line.append(0)
+        for item in row:
+            grid_line.append(item)
+        grid_line.append(0)
+    i = 0
+    while i < row_len:
+        grid_line.append(0)
+        i += 1
+
+    for item in grid_line:
+        if item == 1:
+            if grid_line[index - row_len] == 0:
+                count += 1
+            if grid_line[index - 1] == 0:
+                count += 1
+            if grid_line[index + 1] == 0:
+                count += 1
+            if grid_line[index + row_len] == 0:
+                count += 1
+        index += 1
+
+    return count
--- 0x1C-makefiles/main.c
+++ 0x1C-makefiles/main.c
@@ -1,13 +1,12 @@
 #include "m.h"
 
 /**
- * main - Entry point
- *
- * Return: Always
- */
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 
+ *     */
 int main(void)
 {
-	print_holberton();
-	return (EXIT_SUCCESS);
+		print_school();
+			return (EXIT_SUCCESS);
 }
-/* Holberton */
